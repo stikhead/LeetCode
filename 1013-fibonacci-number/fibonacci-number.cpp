8d89a2c97556400ae6fd10983916fa48d2f6a695
stickhead <120650272+stikhead@users.noreply.github.com>
2026-01-17 18:13:27 +0530

    Time: 6 ms (50.97%) | Memory: 7.9 MB (36.22%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
-    int fib(int n) {
-        long long f0 = 0;
-        long long f1 = 1;
-        if(n<=1) return n;
-        long long f2;
-        for(int i=2; i<=n; i++){
-            f2 = f1 + f0;
-            f0 = f1;
-            f1 = f2;
+    int fibSeries(int n){
+        if(n<=1){
+            return n;
         }
-        return f1;
+        return fibSeries(n - 1) + fibSeries(n-2);
     }
+    int fib(int n) {
+        return fibSeries(n);
+    }
+    // int fib(int n) {
+    //     long long f0 = 0;
+    //     long long f1 = 1;
+    //     if(n<=1) return n;
+    //     long long f2;
+    //     for(int i=2; i<=n; i++){
+    //         f2 = f1 + f0;
+    //         f0 = f1;
+    //         f1 = f2;
+    //     }
+    //     return f1;
+    // }
 };
