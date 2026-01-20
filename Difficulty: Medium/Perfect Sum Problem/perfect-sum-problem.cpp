4a92cc1f1332e9d8fb52b456de38f903e5778924
stickhead <120650272+stikhead@users.noreply.github.com>
2026-01-20 12:40:34 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -21,7 +21,18 @@ class Solution {
     
     int perfectSum(vector<int>& arr, int target) {
         int size = arr.size();
-        vector<vector<int>> dp(size, vector<int>(target+1, -1));
-        return recur(arr, dp, target, arr.size());
+        vector<vector<int>> dp(size+1, vector<int>(target+1));
+        dp[0][0] = 1;
+        for(int i=1; i<size+1; i++){
+            for(int j=0; j<target+1; j++){
+                if(arr[i-1]<=j){
+                    dp[i][j] = dp[i-1][j - arr[i-1]] + dp[i-1][j];
+                }
+                else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[size][target];
     }
 };
