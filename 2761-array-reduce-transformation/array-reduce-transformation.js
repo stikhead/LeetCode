6b95075aa6ccb109d39d198c1d93497a466a7c15
stickhead <120650272+stikhead@users.noreply.github.com>
2026-04-03 18:30:19 +0530

    Time: 54 ms (12.88%) | Memory: 54.6 MB (21.56%) - LeetSync

---
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let val = init;
+    nums.map((ele) => val = fn(val, ele ));
+    return val
+};
