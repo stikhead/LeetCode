5e0c23c809402718de005560969f96c3fda5bdcd
stickhead <120650272+stikhead@users.noreply.github.com>
2026-04-03 17:46:53 +0530

    Time: 51 ms (10.93%) | Memory: 53.3 MB (77.06%) - LeetSync

--- 2734-array-prototype-last/array-prototype-last.js
+++ 2734-array-prototype-last/array-prototype-last.js
@@ -2,10 +2,7 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function() {
-    if(this.length==0){
-        return -1;
-    }
-    return this[this.length-1];
+  return this.length ? this.slice(-1)[0] : -1;
 };
 
 /**
