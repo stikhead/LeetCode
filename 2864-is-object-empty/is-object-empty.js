56d8af1b1eb6b475401ac33227d901b780507f3b
stickhead <120650272+stikhead@users.noreply.github.com>
2026-04-04 15:40:51 +0530

    Time: 38 ms (83.83%) | Memory: 53.6 MB (83.22%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -3,6 +3,10 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-    let i = Object.keys(obj)
-    return i.length===0 ? true : false
+    for (const key in obj) {
+        return false;
+    }
+    return true;
+    // let i = Object.keys(obj)
+    // return i.length===0 ? true : false
 };
