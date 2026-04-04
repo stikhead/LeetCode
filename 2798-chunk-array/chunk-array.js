0a155fd2b5c6667079d761e1a5288672c0e5976b
stickhead <120650272+stikhead@users.noreply.github.com>
2026-04-04 13:46:24 +0530

    Time: 187 ms (5.77%) | Memory: 63.8 MB (5.61%) - LeetSync

---
+++ 2798-chunk-array/chunk-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    let chunkedArr = [];
+    if(size<=arr.size){
+        return chunkedArr.push(arr);
+    } else {
+        let tempArr = [];
+        let count = 0;
+        for(let i = 0; i<arr.length; i++){
+            tempArr = [...tempArr, arr[i]];
+            count++;
+            if(count===size){
+                chunkedArr.push(tempArr);
+                tempArr = [];
+                count = 0;
+            }
+        }
+        if(tempArr.length>0){
+                chunkedArr.push(tempArr)
+            }
+        return chunkedArr
+
+        
+    }
+};
