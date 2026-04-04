1b2c9cdb3ffae1216a75cd80fe9bc482c626e61f
stickhead <120650272+stikhead@users.noreply.github.com>
2026-04-04 13:49:28 +0530

    Time: 48 ms (48.97%) | Memory: 56.2 MB (55.42%) - LeetSync

--- 2798-chunk-array/chunk-array.js
+++ 2798-chunk-array/chunk-array.js
@@ -5,25 +5,27 @@
  */
 var chunk = function(arr, size) {
     let chunkedArr = [];
-    if(size<=arr.size){
-        return chunkedArr.push(arr);
-    } else {
-        let tempArr = [];
-        let count = 0;
-        for(let i = 0; i<arr.length; i++){
-            tempArr = [...tempArr, arr[i]];
-            count++;
-            if(count===size){
-                chunkedArr.push(tempArr);
-                tempArr = [];
-                count = 0;
-            }
-        }
-        if(tempArr.length>0){
-                chunkedArr.push(tempArr)
-            }
-        return chunkedArr
-
-        
+    for(let i=0; i<arr.length; i+=size){
+        chunkedArr.push(arr.slice(i, i+size));
     }
+    return chunkedArr
+    // if(size<=arr.size){
+    //     return chunkedArr.push(arr);
+    // } else {
+    //     let tempArr = [];
+    //     let count = 0;
+    //     for(let i = 0; i<arr.length; i++){
+    //         tempArr = [...tempArr, arr[i]];
+    //         count++;
+    //         if(count===size){
+    //             chunkedArr.push(tempArr);
+    //             tempArr = [];
+    //             count = 0;
+    //         }
+    //     }
+    //     if(tempArr.length>0){
+    //             chunkedArr.push(tempArr)
+    //         }
+    //     return chunkedArr
+    // }
 };
