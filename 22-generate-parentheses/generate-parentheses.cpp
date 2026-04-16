ec4b3ec2b44b82bf324b9b47fb14283fd9f48b0f
stickhead <120650272+stikhead@users.noreply.github.com>
2026-04-16 11:42:01 +0530

    Time: 1 ms (85.51%) | Memory: 12.8 MB (94.62%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,46 +1,75 @@
 class Solution {
 public:
-    void recursion(vector<string> &output, int n, int openIndex, int closeIndex, string &open, string &close, string &op){
+    void recursion(vector<string> &output, int openIndex, int closeIndex, int n, string &op){
         if(openIndex == n && closeIndex == n){
             output.push_back(op);
             return;
-        } 
-
-        if(openIndex == closeIndex){
-            op.push_back(open[openIndex]);
-            recursion(output, n, openIndex+1, closeIndex, open, close, op);
-            op.pop_back();
-            return;
         }
 
-        if(openIndex == n && closeIndex < n){
-            op.push_back(close[closeIndex]);
-            recursion(output, n, openIndex, closeIndex+1, open, close, op);
+        if(openIndex < n){
+            op.push_back('(');
+            recursion(output, openIndex + 1, closeIndex, n, op);
             op.pop_back();
-            return;
         }
 
-        op.push_back(open[openIndex]);
-        recursion(output, n, openIndex + 1, closeIndex, open , close, op);
-        op.pop_back();
-        
-        op.push_back(close[closeIndex]);
-        recursion(output, n, openIndex, closeIndex +1, open , close, op);
-        op.pop_back();
-
-        return;
-
+        if(closeIndex < openIndex){
+            op.push_back(')');
+            recursion(output, openIndex, closeIndex + 1, n, op);
+            op.pop_back();
+        }
     }
     vector<string> generateParenthesis(int n) {
-       string open = "";
-       string close = "";
-       for(int i=0; i<n; i++){
-        open.push_back('(');
-        close.push_back(')');
-       }
         vector<string> output = {};
         string op = "";
-       recursion(output, n, 0, 0, open, close, op);
-       return output;
+        recursion(output, 0, 0, n, op);
+        return output;
     }
 };
+
+
+// class Solution {
+// public:
+//     void recursion(vector<string> &output, int n, int openIndex, int closeIndex, string &open, string &close, string &op){
+//         if(openIndex == n && closeIndex == n){
+//             output.push_back(op);
+//             return;
+//         } 
+
+//         if(openIndex == closeIndex){
+//             op.push_back(open[openIndex]);
+//             recursion(output, n, openIndex+1, closeIndex, open, close, op);
+//             op.pop_back();
+//             return;
+//         }
+
+//         if(openIndex == n && closeIndex < n){
+//             op.push_back(close[closeIndex]);
+//             recursion(output, n, openIndex, closeIndex+1, open, close, op);
+//             op.pop_back();
+//             return;
+//         }
+
+//         op.push_back(open[openIndex]);
+//         recursion(output, n, openIndex + 1, closeIndex, open , close, op);
+//         op.pop_back();
+        
+//         op.push_back(close[closeIndex]);
+//         recursion(output, n, openIndex, closeIndex +1, open , close, op);
+//         op.pop_back();
+
+//         return;
+
+//     }
+//     vector<string> generateParenthesis(int n) {
+//        string open = "";
+//        string close = "";
+//        for(int i=0; i<n; i++){
+//         open.push_back('(');
+//         close.push_back(')');
+//        }
+//         vector<string> output = {};
+//         string op = "";
+//        recursion(output, n, 0, 0, open, close, op);
+//        return output;
+//     }
+// };
