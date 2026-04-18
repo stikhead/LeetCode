0d29696cca38c67f996d423fac523b32b931ad9b
stickhead <120650272+stikhead@users.noreply.github.com>
2026-04-18 11:25:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Permutations of a String/permutations-of-a-string.cpp
+++ Difficulty: Medium/Permutations of a String/permutations-of-a-string.cpp
@@ -1,23 +1,40 @@
 class Solution {
   public:
-   void recursion(vector<string> &output, string input, string op){
-       if(input.size()==0){
-           output.push_back(op);
-           return;
-       }
-       unordered_set<char> mp;
-       for(int i=0; i<input.size(); i++){
+//   void recursion(vector<string> &output, string input, string op){
+//       if(input.size()==0){
+//           output.push_back(op);
+//           return;
+//       }
+//       unordered_set<char> mp;
+//       for(int i=0; i<input.size(); i++){
+//             if(mp.find(input[i])==mp.end()){
+//                 mp.insert(input[i]);
+//                 string newInput = input.substr(0, i) + input.substr(i+1, input.size());
+//                 string newOutput = op + input[i];
+//                 recursion(output, newInput, newOutput);
+//             }
+//       }
+//   }
+
+    void backtracking(vector<string> &output,string &input, int index){
+        if(index==input.size()-1){
+            output.push_back(input);
+            return; 
+        }
+        
+        unordered_set<char> mp;
+        for(int i=index; i<input.size(); i++){
             if(mp.find(input[i])==mp.end()){
                 mp.insert(input[i]);
-                string newInput = input.substr(0, i) + input.substr(i+1, input.size());
-                string newOutput = op + input[i];
-                recursion(output, newInput, newOutput);
+                swap(input[i], input[index]);
+                backtracking(output, input, index+1);
+                swap(input[i], input[index]);
             }
-       }
-   }
+        }
+    }
     vector<string> findPermutation(string &s) {
         vector<string> output = {};
-        recursion(output, s, "");
+        backtracking(output, s, 0);
         return output;
     }
 };
