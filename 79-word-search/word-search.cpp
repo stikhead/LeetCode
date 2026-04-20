b9d46151ef5113f12f8085a1aed2b4c95c3ee06a
stickhead <120650272+stikhead@users.noreply.github.com>
2026-04-20 21:58:06 +0530

    Time: 1085 ms (6.96%) | Memory: 11.8 MB (19.13%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,45 +1,43 @@
 class Solution {
 public:
-    void recursion(vector<vector<char>>& board, vector<vector<int>>& visited,
-                   string word, int r, int c, int index, string& output,
-                   string& ans) {
+    bool recursion(vector<vector<char>>& board, vector<vector<int>>& visited,
+                   string word, int r, int c, int index, string& output) {
         if (output == word) {
-            ans = output;
-            return;
+            return true;
         }
+
         if (r >= 0 && r < board.size() && c >= 0 && c < board[0].size()) {
             if (!visited[r][c] && index < word.size() &&
                 word[index] == board[r][c]) {
                 output.push_back(board[r][c]);
                 visited[r][c] = 1;
                 index += 1;
-                recursion(board, visited, word, r, c + 1, index, output, ans);
-                recursion(board, visited, word, r, c - 1, index, output, ans);
-                recursion(board, visited, word, r + 1, c, index, output, ans);
-                recursion(board, visited, word, r - 1, c, index, output, ans);
+                bool found =  recursion(board, visited, word, r, c + 1, index, output) ||
+                              recursion(board, visited, word, r, c - 1, index, output) ||
+                              recursion(board, visited, word, r + 1, c, index, output) ||
+                              recursion(board, visited, word, r - 1, c, index, output);
+                if(found){
+                    return true;
+                }
                 index--;
                 output.pop_back();
                 visited[r][c] = 0;
             }
         }
+        return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int m =board.size();
+        int m = board.size();
         int n = board[0].size();
         string output = "";
         string ans = "";
         vector<vector<int>> visited(m, vector<int>(n, 0));
-        
-        vector<vector<int>> swap(m, vector<int>(n, 0));
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (word[0] == board[i][j]) {
-                    recursion(board, visited, word, i, j, 0, output, ans);
-                    if (word == ans) {
-                        cout << ans;
+                    if(recursion(board, visited, word, i, j, 0, output)){
                         return true;
                     }
-                    visited = swap;
                     ans = "";
                 }
             }
