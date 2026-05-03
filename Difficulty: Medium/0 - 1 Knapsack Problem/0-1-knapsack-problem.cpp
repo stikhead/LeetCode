6aef297d23ea648397b2da265d3a20604146ca52
stickhead <120650272+stikhead@users.noreply.github.com>
2026-05-03 23:42:13 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -15,11 +15,31 @@ class Solution {
         
         return dp[index][remaining_weight] = max(skip, take);
     }
+    
+    
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
         int s = W;
-        vector<vector<int>> dp(val.size()+1, vector<int>(W+1, -1));
+        int n = val.size();
+        vector<vector<int>> dp(n+1, vector<int>(W+1, -1));
         
-        return recursion(dp, val, wt, 0, s);
+        // return recursion(dp, val, wt, 0, s);
+        for(int i=0; i<=W; i++){
+            dp[0][i] = 0;
+        }
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 0;
+        }
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=W; j++){
+                if(wt[i-1]<=j){
+                    dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][W];
         
     }
 };
