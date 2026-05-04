36a48ad7b67eb51dc359a7c7af00ee8bd8fb0492
stickhead <120650272+stikhead@users.noreply.github.com>
2026-05-04 22:10:33 +0530

    Time: 3 ms (7.77%) | Memory: 11.7 MB (7.27%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,26 +1,49 @@
+// class Solution {
+// public:
+//     int recursion(vector<int> &dp, vector<int> &nums, int n, int index, int end){
+//         if(index> end){
+//             return 0;
+//         }
+//         if(dp[index]!=-1) return dp[index];
+
+//         int rob = nums[index] + recursion(dp, nums, n, index+2, end);
+     
+//         int dontrob = recursion(dp, nums, n, index+1, end);
+
+//         return dp[index] = max(rob, dontrob);
+//     }
+//     int rob(vector<int>& nums) {
+//         if(nums.size()==1){
+//             return nums[0];
+//         }
+//         vector<int> dpA(nums.size(), -1);
+//         vector<int> dpB(nums.size(), -1);
+//         int a =  recursion(dpA, nums, nums.size(), 0, nums.size()-2);
+         
+//         int b =  recursion(dpB, nums, nums.size(), 1, nums.size()-1);
+//         return max(a, b);
+//     }
+// };
+
 class Solution {
 public:
-    int recursion(vector<int> &dp, vector<int> &nums, int n, int index, int end){
-        if(index> end){
-            return 0;
-        }
-        if(dp[index]!=-1) return dp[index];
+    int recursion(vector<vector<int>> &dp, vector<int> &nums, int n, int index, bool flag){
+        if(index>=n){
+            return 0;
+        }
+        if(dp[index][flag]!=-1) return dp[index][flag];
+        if((index+1)%n==0 && flag){
+            return 0;
+        }
+        bool didrob = index==0 ? true  : flag;
+        int rob = nums[index] + recursion(dp, nums, n, index+2, didrob);
+        int dontrob = recursion(dp, nums, n, index+1, flag);
 
-        int rob = nums[index] + recursion(dp, nums, n, index+2, end);
-     
-        int dontrob = recursion(dp, nums, n, index+1, end);
-
-        return dp[index] = max(rob, dontrob);
-    }
-    int rob(vector<int>& nums) {
-        if(nums.size()==1){
-            return nums[0];
-        }
-        vector<int> dpA(nums.size(), -1);
-        vector<int> dpB(nums.size(), -1);
-        int a =  recursion(dpA, nums, nums.size(), 0, nums.size()-2);
-         
-        int b =  recursion(dpB, nums, nums.size(), 1, nums.size()-1);
-        return max(a, b);
-    }
+        return dp[index][flag] = max(rob, dontrob);
+    }
+    int rob(vector<int>& nums) {
+        bool first = false;
+        vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), -1));
+        return recursion(dp, nums, nums.size(), 0, false);
+    }
 };
