ff10ed9b47f06426160f73a6c9a8bbc797767257
stickhead <120650272+stikhead@users.noreply.github.com>
2026-05-05 16:41:54 +0530

    Time: 3 ms (28.63%) | Memory: 16.2 MB (57.17%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+
+    int recursion(vector<vector<int>> &dp, vector<vector<int>>& grid, int m, int n, int r, int c){
+        if(r==m-1 && c==n-1) return grid[r][c];
+        if(r>=m || c>=n) return 1e9;
+        if(dp[r][c]!=-1) return dp[r][c];
+        int down = grid[r][c] + recursion(dp, grid, m, n, r+1, c);
+        int right = grid[r][c] + recursion(dp, grid, m, n, r, c+1);
+        return dp[r][c] = min(down, right);
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), -1));
+     return recursion(dp, grid, grid.size(), grid[0].size(), 0, 0);
+    }
+};
