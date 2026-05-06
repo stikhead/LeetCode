db3669ab57f30e3679621b0d2183caa319d8273f
stickhead <120650272+stikhead@users.noreply.github.com>
2026-05-06 21:26:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -1,38 +1,28 @@
 class Solution {
   public:
-    int recur(vector<int>& arr, vector<vector<int>> &dp, int target, int n){
-        if(target==0 && n==0){
-            return 1;
-        }
-        if(n==0){
+    int recursion(vector<vector<int>> &dp, vector<int> &arr, int target, int sum, int index){
+       
+        if(sum>target){
             return 0;
         }
-        if(dp[n-1][target]!=-1){
-            return dp[n-1][target];
-        }
-        if(arr[n-1]<=target){
-            return dp[n-1][target] = recur(arr, dp, target, n-1) + recur(arr, dp, target - arr[n-1], n-1);
-        }
-        else {
-            return dp[n-1][target] = recur(arr, dp, target, n-1);
+       if(index == arr.size()){
+            if(sum == target) return 1;
+            else return 0;
         }
         
+        if(dp[index][sum]!=-1) return dp[index][sum];
+        
+        
+        return dp[index][sum] = recursion(dp, arr, target, sum+arr[index], index+1) + recursion(dp, arr, target, sum, index + 1);
     }
-    
     int perfectSum(vector<int>& arr, int target) {
-        int size = arr.size();
-        vector<vector<int>> dp(size+1, vector<int>(target+1));
-        dp[0][0] = 1;
-        for(int i=1; i<size+1; i++){
-            for(int j=0; j<target+1; j++){
-                if(arr[i-1]<=j){
-                    dp[i][j] = dp[i-1][j - arr[i-1]] + dp[i-1][j];
-                }
-                else {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        return dp[size][target];
+        // if(arr.size()==1){
+        //     if(arr[0]==target) return 1;
+        //     else return 0;
+        // }
+        
+        vector<vector<int>> dp(arr.size(), vector<int>(target+1, -1));
+        return recursion(dp, arr, target, 0, 0);
+        
     }
 };
