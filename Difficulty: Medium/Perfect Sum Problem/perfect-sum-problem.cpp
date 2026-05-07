ed85acdd769fbdb8c8b385a25eb2c121c86ca189
stickhead <120650272+stikhead@users.noreply.github.com>
2026-05-07 23:25:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -2,27 +2,31 @@ class Solution {
   public:
     int recursion(vector<vector<int>> &dp, vector<int> &arr, int target, int sum, int index){
        
-        if(sum>target){
+    
+       if(index < 0){
+           if(sum==target){
+                return 1;
+            }
             return 0;
-        }
-       if(index == arr.size()){
-            if(sum == target) return 1;
-            else return 0;
+       }
+       if(sum>target){
+            return 0;
         }
         
         if(dp[index][sum]!=-1) return dp[index][sum];
         
+        int pick = 0;
+        if(sum+arr[index]<=target){
+             pick = recursion(dp, arr, target, sum+arr[index], index-1);
+        }
         
-        return dp[index][sum] = recursion(dp, arr, target, sum+arr[index], index+1) + recursion(dp, arr, target, sum, index + 1);
+        int nopick =  recursion(dp, arr, target, sum, index - 1);
+        return dp[index][sum] = pick + nopick;
     }
     int perfectSum(vector<int>& arr, int target) {
-        // if(arr.size()==1){
-        //     if(arr[0]==target) return 1;
-        //     else return 0;
-        // }
         
         vector<vector<int>> dp(arr.size(), vector<int>(target+1, -1));
-        return recursion(dp, arr, target, 0, 0);
+        return recursion(dp, arr, target, 0, arr.size()-1);
         
     }
 };
