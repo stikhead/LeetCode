dc1573a5fd33fe7b2c81c8df1ec2e751f6fc4547
stickhead <120650272+stikhead@users.noreply.github.com>
2026-05-09 18:36:47 +0530

    Time: 43 ms (21.29%) | Memory: 27.6 MB (26.40%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int recursion(vector<vector<int>> &dp, string &s1, string &s2, int index1, int index2){
+        if(index1<0 || index2<0){
+            return 0;
+        }
+        
+        if(dp[index1][index2]!=-1) return dp[index1][index2];
+        int left = 0;
+        int right = 0;
+        
+       
+        if(s1[index1]!=s2[index2]){
+            left = recursion(dp, s1, s2, index1, index2-1);
+            right = recursion(dp, s1, s2, index1-1, index2);
+        } else {
+            return 1 + recursion(dp, s1, s2, index1-1, index2-1);
+        } 
+        
+        return dp[index1][index2] = max(left, right);
+        
+    
+        
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>> dp(text1.size(), vector<int>(text2.size(), -1));
+        return recursion(dp, text1, text2, text1.size()-1, text2.size()-1);
+    }
+};
