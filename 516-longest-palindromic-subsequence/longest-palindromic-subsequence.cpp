9d1612e060dd7fa2a0a59d6c66c0a6eb5dff3b68
stickhead <120650272+stikhead@users.noreply.github.com>
2026-05-11 17:43:36 +0530

    Time: 47 ms (89.27%) | Memory: 76 MB (20.86%) - LeetSync

---
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public: 
+     int recursion(vector<vector<int>> &dp, string &s1, int index1, int index2){
+        if(index1> index2){
+            return 0;
+        }
+        
+        if(index1==index2) return 1;
+        
+        if(dp[index1][index2]!=-1) return dp[index1][index2];
+        int left = 0;
+        int right = 0;
+        
+        if(s1[index1]==s1[index2]) {
+            return dp[index1][index2] = 2 + recursion(dp, s1, index1+1, index2-1);
+        } 
+        if(s1[index1]!=s1[index2]){
+            left = recursion(dp, s1, index1, index2-1);
+            right = recursion(dp, s1, index1+1, index2);
+        } 
+        
+        return dp[index1][index2] = max(left, right);
+        
+    }
+    int longestPalindromeSubseq(string s) {
+         vector<vector<int>> dp(s.size(), vector<int>(s.size(), -1));
+        return recursion(dp, s, 0, s.size()-1);
+    }
+};
