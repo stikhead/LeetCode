fe54a44e69a10a3b58c0cd967e78e97fdbbc6afb
stickhead <120650272+stikhead@users.noreply.github.com>
2026-05-12 20:46:03 +0530

    Time: 56 ms (11.79%) | Memory: 31.6 MB (15.91%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int recursion(vector<vector<int>> &dp, string &s, int index1, int index2){
+        if(index1<0) return 0;
+        if(index2>s.size()) return 0; 
+        if(dp[index1][index2]!=-1) return dp[index1][index2];
+        if(s[index1]==s[index2]){
+            return dp[index1][index2] = 1 + recursion(dp, s, index1-1,index2+1);
+        }
+        int left = recursion(dp, s, index1-1, index2);
+        int right = recursion(dp, s, index1, index2+1);
+        return dp[index1][index2] = max(left, right);
+    }
+    int minInsertions(string s) {
+        vector<vector<int>> dp(s.size()+1, vector<int>(s.size()+1,-1));
+        // string rev = s;
+        // reverse(s.begin(), s.end());
+        return s.size() - recursion(dp, s, s.size()-1, 0);
+    }
+};
