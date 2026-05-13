c5a947c7f4b9d0bbad8aab8e107fdb4a07c0b0f2
stickhead <120650272+stikhead@users.noreply.github.com>
2026-05-13 18:03:38 +0530

    Time: 375 ms (45.88%) | Memory: 9.4 MB (77.80%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,51 +1,33 @@
 class Solution {
 public:
-    // Palindrome helper remains exactly the same
-    bool isPalindrome(const string &s, int left, int right){
-        while(left < right){
-            if(s[left] != s[right]) return false;
-            left++;
-            right--;
+    bool IsPalindrome(string &s, int i, int j){
+        while(i<=j){
+            if(s[i]!=s[j]) return false;
+            i++;
+            j--;
         }
         return true;
     }
-
-    // Now returns the minimum cuts required from 'index' to the end of the string
-    int solve(const string &input, int index, vector<int> &memo) {
-        // Base case: If we reach the end, no more cuts are needed
-        if (index == input.size()) {
-            return 0;
-        }
-
-        // If we've already calculated the answer for this suffix, return it instantly
-        if (memo[index] != -1) {
-            return memo[index];
-        }
-
-        int min_cuts = INT_MAX;
-
-        // Try all possible prefixes starting from 'index'
-        for (int i = index; i < input.size(); i++) {
-            if (isPalindrome(input, index, i)) {
-                // If s[index...i] is a palindrome, we make 1 cut here.
-                // Then we recursively find the min cuts for the remaining string (i + 1)
-                int cuts_for_rest = solve(input, i + 1, memo);
-                
-                // We add 1 to represent the cut we just made (unless we are at the very end of the string)
-                int total_cuts = (i == input.size() - 1) ? 0 : 1 + cuts_for_rest;
-                
-                min_cuts = min(min_cuts, total_cuts);
+    
+    int recursion(vector<int> &dp, string &s, int index){
+        if(index<=0) return 0;
+        
+        if(dp[index]!=-1) return dp[index];
+        if(IsPalindrome(s, 0, index)) return dp[index] = 0;
+        int ans = INT_MAX;
+        for(int k=1; k<=index; k++){
+            if(IsPalindrome(s, k, index)){
+                int temp = recursion(dp, s, k-1) + 1;
+                ans = min(temp,ans);
             }
         }
-
-        // Store the result before returning
-        return memo[index] = min_cuts;
+    
+        return dp[index] = ans;
     }
-
+    
     int minCut(string s) {
-        int n = s.size();
-        // Initialize memoization array with -1
-        vector<int> memo(n, -1);
-        return solve(s, 0, memo);
+        vector<int> dp(s.size()+1, -1);
+      
+        return recursion(dp, s, s.size()-1);
     }
 };
