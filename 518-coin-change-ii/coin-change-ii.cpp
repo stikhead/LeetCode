d2d3b573c19e42cb82ffc3e46ea282ffcf777101
stickhead <120650272+stikhead@users.noreply.github.com>
2026-05-14 20:00:16 +0530

    Time: 27 ms (63.86%) | Memory: 36.7 MB (46.92%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,34 +1,17 @@
 class Solution {
 public:
-    int recur(vector<int> &coins, vector<vector<int>> &dp, int amount, int n){
-        if(amount==0){
-            return 1;
-        }
-        if(n==0){
-            return 0;
-        }
-        // if(n==1){
-        //     if(amount%coins[n-1]==0){
-        //         return amount/coins[n-1];
-        //     }
-        //     else {
-        //         return INT_MAX - 1;
-        //     }
-        // }
-        if(dp[n-1][amount]!=-1){
-            return dp[n-1][amount];
-        }
-        if(coins[n-1]<=amount){
-            return dp[n-1][amount] = recur(coins, dp, amount - coins[n-1], n) + recur(coins, dp, amount, n-1); 
-        }
-        else {
-            return dp[n-1][amount] = recur(coins, dp, amount, n-1);
-        }
+    int recursion(vector<vector<int>> &dp, vector<int>&coins, int index, int amount){
+        if(amount==0) return 1;
+        if(index<0) return 0;
+        if(dp[index][amount]!=-1) return dp[index][amount];
+        int left = 0;
+        if(amount-coins[index]>= 0) left = recursion(dp, coins, index, amount-coins[index]);
+        
+        int right = recursion(dp, coins, index-1, amount);
+        return dp[index][amount] = left+right;
     }
     int change(int amount, vector<int>& coins) {
         vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
-        int res = recur(coins, dp, amount, coins.size());
-        if(res == INT_MAX-1) return 0;
-        return res;
+        return recursion(dp, coins, coins.size()-1, amount);
     }
 };
