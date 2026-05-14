9fde10c26e66caa9fc6a76a9d996711a8fe0dd34
stickhead <120650272+stikhead@users.noreply.github.com>
2026-05-14 20:35:24 +0530

    Time: 43 ms (37.61%) | Memory: 53 MB (13.54%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -11,8 +11,24 @@ public:
         return dp[index][amount] = min(left,right);
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
-        int sol = recursion(dp, coins, coins.size()-1, amount);
-        return sol!=1e9 ? sol : -1;
+        // vector<vector<int>> dp(coins.size(), vector<int>(amount+1, -1));
+        // int sol = recursion(dp, coins, coins.size()-1, amount);
+        // return sol!=1e9 ? sol : -1;
+
+        vector<vector<int>> dp(coins.size()+1, vector<int>(amount+1, 1e9));
+        for(int i=0; i<=coins.size(); i++){
+            dp[i][0] = 0;
+        }
+
+        for(int i=1; i<=coins.size(); i++){
+            for(int j=1; j<=amount; j++){
+                if(j<coins[i-1]){
+                    dp[i][j]  = dp[i-1][j];
+                } else {
+                    dp[i][j] = min(dp[i-1][j], 1 + dp[i][j-coins[i-1]]);
+                }
+            }
+        }
+           return dp[coins.size()][amount] >= 1e9 ? -1 : dp[coins.size()][amount];
     }
 };
