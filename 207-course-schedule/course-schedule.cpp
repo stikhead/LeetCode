fafa63af7f2a38a163f91347f4d26cdd5c61725d
stickhead <120650272+stikhead@users.noreply.github.com>
2026-06-05 12:39:44 +0530

    Time: 31 ms (5.68%) | Memory: 19.3 MB (53.92%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    bool dfs(vector<vector<int>> &adjList, vector<int> &vis, int node){
+        if(vis[node]==1){
+            return true;
+        }
+        if(vis[node]==2){
+            return false;
+        }
+        vis[node] = 1;
+        for(int i=0; i<adjList[node].size(); i++){
+            if(dfs(adjList, vis, adjList[node][i])){
+                return true;
+            }
+        }
+        vis[node] = 2;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjList(numCourses);
+        for(int i=0; i<prerequisites.size(); i++){
+            adjList[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+
+        vector<int> vis(numCourses, 0);
+        for(int i=0; i<adjList.size(); i++){
+            for(int j=0; j<adjList[i].size(); j++){
+                cout<<adjList[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        for(int i=0; i<numCourses; i++){
+            if(dfs(adjList, vis, i)){
+                return false;
+            }
+        }
+
+        return true;
+
+
+    }
+};
