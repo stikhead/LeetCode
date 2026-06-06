ee81dc26a538d7a46009f3d7a5f00c3b03736902
stickhead <120650272+stikhead@users.noreply.github.com>
2026-06-06 17:13:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Steps by Knight/steps-by-knight.cpp
+++ Difficulty: Medium/Steps by Knight/steps-by-knight.cpp
@@ -1,39 +1,39 @@
 class Solution {
-  public:
-    int minStepToReachTarget(vector<int>& knightPos, vector<int>& targetPos, int n) {
-        // Code here
-        vector<pair<int, int>> dir = {
-            {1, 2}, {2, 1}, {-1, 2}, {-2, 1}, {1, -2}, {2, -1}, {-1, -2}, {-2, -1}
-        };
-        
-        queue<pair<int, int>> q;
-        q.push({knightPos[0], knightPos[1]});
-        int minStep = 0;
-        vector<vector<int>> visited(n+1, vector<int>(n+1, 0));
-        visited[knightPos[0]][knightPos[1]] = 1;
-        while(!q.empty()){
-            int size = q.size();
-            minStep++;
-            for(int i=0; i<size; i++){
-                auto [row, col] = q.front();
-                
-                q.pop();
-                for(int j=0; j<8; j++){
-                    int nrow = row + dir[j].first;
-                    int ncol = col + dir[j].second;
-                    
-                    if(nrow>0 && nrow<=n && ncol >0 && ncol <=n){
-                        if(targetPos[0]==nrow && targetPos[1]==ncol){
-                           return minStep;
-                        }
-                        if(!visited[nrow][ncol]){
-                            q.push({nrow, ncol});
-                            visited[nrow][ncol] = 1;
-                        }
-                    }
-                }
-            }
-        }
-        return 0;
-    }
+	public:
+	int minStepToReachTarget(vector<int>& knightPos, vector<int>& targetPos, int n) {
+		vector<pair<int, int>> dir = {{-2, -1}, {-1, -2}, {1, -2}, {2, -1},
+		{-2, 1}, {-1, 2}, {1, 2}, {2, 1}};
+		queue<pair<int, int>> q;
+		vector<vector<bool>> vis(n + 1, vector<bool>(n + 1, false));
+		q.push({knightPos[0], knightPos[1]});
+		int steps = 0;
+		vis[knightPos[0]][knightPos[1]] = true;
+		while (!q.empty()) {
+			int size = q.size();
+			steps++;
+			for (int i = 0; i<size; i++) {
+				auto [row, col] = q.front();
+				q.pop();
+				for (int j = 0; j<8; j++) {
+					int nrow = row + dir[j].first;
+					int ncol = col + dir[j].second;
+					
+					if (nrow > 0 && nrow <= n && ncol <= n && ncol > 0) {
+						if (nrow == targetPos[0] && ncol == targetPos[1]) {
+							return steps;
+						}
+						if (vis[nrow][ncol] == false) {
+							q.push({nrow, ncol});
+				            vis[nrow][ncol] = true;
+							
+						}
+						
+					}
+				}
+			}
+		}
+		
+		return 0;
+		
+	}
 };
