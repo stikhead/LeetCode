2c680565adc39c9bd0147868e4dc3e519b5cd0cb
stickhead <120650272+stikhead@users.noreply.github.com>
2026-06-07 13:41:02 +0530

    Time: 2 ms (95.03%) | Memory: 51.9 MB (83.27%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    bool dfs(vector<int> &safeNodes, vector<vector<int>> &list, vector<int> &vis, int node, int outdegree){
+    // bool dfs(vector<int> &safeNodes, vector<vector<int>> &list, vector<int> &vis, int node, int outdegree){
+    //     vis[node] = 1;
+    //     for(int i=0; i<list[node].size(); i++){
+    //         int child = list[node][i];
+    //         if(vis[child]==0){
+    //             if(dfs(safeNodes, list, vis, child, list[child].size())) return true;
+    //         } else if(vis[child]==1){
+    //             return true;
+    //         }
+    //         outdegree--;
+    //     }
+    //     vis[node] = 2;
+    //     if(outdegree==0){
+    //         safeNodes.push_back(node);
+    //     }
+    //     return false;
+        
+    // }
+
+    bool dfs(vector<vector<int>> &list, vector<int> &vis, int node){
         vis[node] = 1;
         for(int i=0; i<list[node].size(); i++){
             int child = list[node][i];
             if(vis[child]==0){
-                if(dfs(safeNodes, list, vis, child, list[child].size())) return true;
+                if(dfs(list, vis, child)) return true;
             } else if(vis[child]==1){
                 return true;
             }
-            outdegree--;
         }
         vis[node] = 2;
-        if(outdegree==0){
-            safeNodes.push_back(node);
-        }
         return false;
-        
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         vector<int> safe;
@@ -25,10 +39,15 @@ public:
         vector<int> vis(V, 0);
         for(int i=0; i<V; i++){
             if(vis[i]==0){
-                dfs(safe, graph, vis, i, graph[i].size());
+                dfs(graph, vis, i);
             }
         }
-        sort(safe.begin(), safe.end());
+        // sort(safe.begin(), safe.end());
+        for(int i = 0; i < V; i++){
+            if(vis[i] == 2){
+                safe.push_back(i);
+            }
+        }
         return safe;
     }
     
