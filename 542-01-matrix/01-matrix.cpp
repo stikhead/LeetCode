fb611d2fe53e5aba988d2b20d810800930ba2c60
stickhead <120650272+stikhead@users.noreply.github.com>
2026-06-08 15:11:31 +0530

    Time: 18 ms (58.21%) | Memory: 34.8 MB (74.96%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -6,6 +6,7 @@ public:
         vector<vector<int>> output(m, vector<int>(n, 0));
         vector<pair<int, int>> dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         queue<pair<int, int>> q;
+        int count = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 0) {
@@ -13,9 +14,12 @@ public:
                 } 
                 if(mat[i][j]==1){
                     output[i][j] = -1;
+                    count++;
                 }
             }
         }
+
+        if(count==0) return output;
         while (!q.empty()) {
             auto [row, col] = q.front();
             q.pop();
