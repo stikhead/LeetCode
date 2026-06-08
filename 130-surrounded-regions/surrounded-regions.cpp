e13c036e360b000084d26d20451d8695a22d242b
stickhead <120650272+stikhead@users.noreply.github.com>
2026-06-08 16:31:03 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (59.19%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>& board, vector<vector<int>>& vis, int i, int j) {
+    void bfs(vector<vector<char>>& board, int i, int j) {
         int m = board.size();
         int n = board[0].size();
 
@@ -9,7 +9,7 @@ public:
 
 
         q.push({i, j});
-        vis[i][j] = 1;
+        board[i][j] = '#';
         while (!q.empty()) {
             auto [row, col] = q.front();
             q.pop();
@@ -18,9 +18,9 @@ public:
                 int ncol = col + dir[i].second;
 
                 if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n) {
-                    if (board[nrow][ncol] == 'O' && vis[nrow][ncol]==0) {
+                    if (board[nrow][ncol] == 'O') {
+                        board[nrow][ncol] = '#';
                         q.push({nrow, ncol});
-                        vis[nrow][ncol] = 1;
                     }
                 }
             }
@@ -29,30 +29,32 @@ public:
     void solve(vector<vector<char>>& board) {
         int m = board.size();
         int n = board[0].size();
-        vector<vector<int>> vis(m, vector<int>(n, 0));
 
         for (int i = 0; i < m; i++) {
-            if (board[i][0] == 'O' &&  vis[i][0]==0) {
-                bfs(board, vis, i, 0);
+            if (board[i][0] == 'O') {
+                bfs(board, i, 0);
             }
-            if (board[i][n-1] == 'O'  && vis[i][n-1]==0) {
-                bfs(board, vis, i, n-1);
+            if (board[i][n-1] == 'O') {
+                bfs(board, i, n-1);
             }
         }
 
         for (int i = 0; i < n; i++) {
-            if (board[0][i] == 'O'  && vis[0][i]==0) {
-                bfs(board, vis, 0, i);
+            if (board[0][i] == 'O') {
+                bfs(board, 0, i);
             }
-            if (board[m-1][i] == 'O'  && vis[m-1][i]==0) {
-                bfs(board, vis, m-1, i);
+            if (board[m-1][i] == 'O') {
+                bfs(board, m-1, i);
             }
         }
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (vis[i][j] == 0 && board[i][j] == 'O') {
+                if (board[i][j] == 'O') {
                     board[i][j] = 'X';
+                }
+                if(board[i][j]=='#'){
+                    board[i][j] = 'O';
                 }
             }
         }
