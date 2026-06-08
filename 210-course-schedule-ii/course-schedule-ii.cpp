9f6f1ccd77aa3b01e73a9b01b96093f45b495b6c
stickhead <120650272+stikhead@users.noreply.github.com>
2026-06-08 12:51:45 +0530

    Time: 12 ms (8.12%) | Memory: 18.3 MB (55.51%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -19,28 +19,60 @@ public:
     }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> list(numCourses);
+        vector<int> vis(numCourses, 0);
+        vector<int> indegree(numCourses);
+        vector<int> ans;
+        queue<int> q;
         for(int i=0; i<prerequisites.size(); i++){
             int a = prerequisites[i][0];
             int b = prerequisites[i][1];
             list[b].push_back(a);
         }
 
-        vector<int> vis(numCourses, 0);
-        // vector<int> indegrees(numCourses);
-        // for(int i=0; i<numCourses; i++){
-        //     for(int j=0; j<list[i].size(); j++){
-        //         indegree[list[i][j]]++;
-        //     }
-        // }
-        vector<int> ans;
-        for(int i=0; i<numCourses; i++){
-            if(vis[i]==0){
-                if(dfs(list, vis, ans, i)){
-                    return {};
+
+        for(int i=0; i<list.size(); i++){
+            for(int j=0; j<list[i].size(); j++){
+                indegree[list[i][j]]++;
+            }
+        }
+
+        for(int i=0; i<indegree.size(); i++){
+            cout<<indegree[i]<<" ";
+            if(indegree[i]==0) {
+                    q.push(i);
+                    ans.push_back(i);
+                };
+        }
+
+        while(!q.empty()){
+            int size = q.size();
+            for(int i=0; i<size; i++){
+                int node = q.front();
+                q.pop();
+                for(int j=0; j<list[node].size(); j++){
+                    int child = list[node][j];
+                    indegree[child]--;
+                    if(indegree[child]==0){
+                        q.push(child);
+                        ans.push_back(child);
+                    }
                 }
             }
         }
-        reverse(ans.begin(), ans.end());
+        for(int i=0; i<indegree.size(); i++){
+            if(indegree[i]!=0){
+                return {};
+            }
+        }
         return ans;
+        // for(int i=0; i<numCourses; i++){
+        //     if(vis[i]==0){
+        //         if(dfs(list, vis, ans, i)){
+        //             return {};
+        //         }
+        //     }
+        // }
+        // reverse(ans.begin(), ans.end());
+        // return ans;
     }
 };
