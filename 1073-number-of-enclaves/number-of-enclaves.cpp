7f250ca9223951503c960a87599cd1bef373b5b8
stickhead <120650272+stikhead@users.noreply.github.com>
2026-06-08 17:02:34 +0530

    Time: 8 ms (62.29%) | Memory: 28.9 MB (94.47%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.cpp
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -3,29 +3,30 @@ public:
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> val(m, vector<int>(n, -1));
-        vector<pair<int, int>> dir = {
-            {0, 1}, {0, -1}, {1, 0}, {-1, 0}
-        };
+        int count  = 0;
+        vector<pair<int, int>> dir = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
         queue<pair<int, int>> q;
-        for(int i=0; i<n; i++){
-            if(grid[0][i]==1 && val[0][i] == -1){
-                q.push({0, i});
-                val[0][i] = 0;
-            } 
-             if(m>1&&grid[m-1][i]==1 && val[m-1][i] == -1){
-                q.push({m-1, i});
-                val[m-1][i] = 0;
-            } 
-        }
         for(int i=0; i<m; i++){
-            if(grid[i][0]==1 && val[i][0] == -1){
+            if(grid[i][0]==1){
                 q.push({i, 0});
-                val[i][0] = 0;
-            } 
-             if(n>1 && grid[i][n-1]==1 && val[i][n-1] == -1){
+                grid[i][0] = 0;
+            }
+
+            if(grid[i][n-1]==1){
                 q.push({i, n-1});
-                val[i][n-1] = 0;
+                grid[i][n-1] = 0;
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            if(grid[0][i]==1){
+                q.push({0, i});
+                grid[0][i] = 0;
+            }
+
+            if(grid[m-1][i]==1){
+                q.push({m-1, i});
+                grid[m-1][i] = 0;
             }
         }
 
@@ -35,22 +36,24 @@ public:
             for(int i=0; i<4; i++){
                 int nrow = row + dir[i].first;
                 int ncol = col + dir[i].second;
-                if(nrow>=0 && ncol >=0 && nrow<m && ncol <n){
-                    if(grid[nrow][ncol] == 1 && val[nrow][ncol]==-1){
-                        val[nrow][ncol] = 0;
+
+                if(nrow>=0 && nrow<m && ncol >=0 && ncol <n){
+                    if(grid[nrow][ncol]==1){
                         q.push({nrow, ncol});
+                        grid[nrow][ncol] = 0;
                     }
                 }
             }
         }
-        int count = 0;
+
         for(int i=1; i<m-1; i++){
             for(int j=1; j<n-1; j++){
-                if(val[i][j]==-1 && grid[i][j]==1){
+                if(grid[i][j]==1){
                     count++;
                 }
             }
         }
-        return count;
+
+    return count;
     }
 };
