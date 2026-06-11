2a67e553bca77415834f83eefe82c1198013ba02
stickhead <120650272+stikhead@users.noreply.github.com>
2026-06-11 19:43:20 +0530

    Time: 55 ms (57.91%) | Memory: 25.6 MB (36.69%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -8,34 +8,30 @@ public:
 
         vector<vector<int>> vis(n, vector<int>(m, INT_MAX));
         vector<pair<int, int>> dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>
-            q;
-        q.push({0, 0, 0});
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> q;
+        q.emplace(0, 0, 0);
         vis[0][0] = 0;
         while (!q.empty()) {
-            auto node = q.top();
-            int row = node[1], col = node[2], cost = node[0];
+            auto [cost, row, col] = q.top();
             q.pop();
 
-            if (row == n - 1 && col == m - 1)
-                return cost;
-            if (cost > vis[row][col])
-                continue;
+            if (cost > vis[row][col]) continue;
+
+            if (row == n - 1 && col == m - 1) return cost;
+
             for (int i = 0; i < 4; i++) {
                 int nrow = row + dir[i].first;
                 int ncol = col + dir[i].second;
 
                 if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m) {
                     // cout << nrow << " " << ncol << endl;
-                    if (abs(heights[nrow][ncol] - heights[row][col]) <
-                        vis[nrow][ncol]) {
-                        int currentEffort =
-                            abs(heights[row][col] - heights[nrow][ncol]);
+                    if (abs(heights[nrow][ncol] - heights[row][col]) < vis[nrow][ncol]) {
+                        int currentEffort = abs(heights[row][col] - heights[nrow][ncol]);
                         int pathEffort = max(cost, currentEffort);
 
                         if (pathEffort < vis[nrow][ncol]) {
                             vis[nrow][ncol] = pathEffort;
-                            q.push({pathEffort, nrow, ncol});
+                            q.emplace(pathEffort, nrow, ncol);
                         }
                     }
                 }
