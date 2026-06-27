c1798a2674aae600b490d8f2353ec33c297272ae
stickhead <120650272+stikhead@users.noreply.github.com>
2026-06-27 19:14:11 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (41.27%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -6,29 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> mp(100);
-        if(!root) return {};
-        queue<pair<TreeNode*, int>> q;
-        q.push({root, 0});
-        int level = 0;
-        while(!q.empty()){
+        vector<int> ans;
+        if (!root) return ans;
+        // queue<pair<TreeNode*, int>> q;
+        // q.push({root, 0});
+        // int level = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
             int size = q.size();
-            level++;
-            for(int i=0; i<size; i++){
-            auto [node, x] = q.front();
-            q.pop();
-            mp[x] = node->val;
-            if(node->left) q.push({node->left, x+1});
-            if(node->right) q.push({node->right, x+1});
+            // level++;
+            for (int i = 0; i < size; i++) {
+                // auto [node, x] = q.front();
+                TreeNode* node = q.front();
+                q.pop();
+                if (i == size - 1) {
+                    ans.push_back(node->val);
+                }
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
             }
         }
-        mp.resize(level);
-        return mp;
+        return ans;
     }
 };
