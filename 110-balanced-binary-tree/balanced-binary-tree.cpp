63adc6222c5dbb27a1ce57109d855b0eaa5d15d1
stickhead <120650272+stikhead@users.noreply.github.com>
2026-06-27 12:36:45 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (20.28%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -17,18 +17,22 @@ public:
             return 0;
         }
         int left = recur(root->left);
+        if(left == -1) return -1;
         int right = recur(root->right);
+        if(right == -1 ) return -1;
+        if(abs(left-right) > 1) return -1;
         return 1 + max(left, right);
     }
     bool isBalanced(TreeNode* root) {
-       if(!root) return true;
-       int left = recur(root->left);
-       int right = recur(root->right);
+    //    if(!root) return true;
+    //    int left = recur(root->left);
+    //    int right = recur(root->right);
 
-       if (abs(left - right) <= 1 && isBalanced(root->left) && isBalanced(root->right)) {
-            return true;
-        }
+    //    if (abs(left - right) <= 1 && isBalanced(root->left) && isBalanced(root->right)) {
+    //         return true;
+    //     }
         
-        return false;
+    //     return false;
+        return recur(root)==-1 ? false : true;
     }
 };
