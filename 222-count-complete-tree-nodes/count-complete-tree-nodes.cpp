c1284c0bc315524552fad2b32f702ef6b175f99c
stickhead <120650272+stikhead@users.noreply.github.com>
2026-06-28 21:41:42 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (16.80%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -13,10 +13,26 @@ class Solution {
 public:
    
     int countNodes(TreeNode* root) {
+        // if(!root) return 0;
+        // if(!root->left && !root->right) return 1;
+        // int left = countNodes(root->left);
+        // int right = countNodes(root->right);
+        // return left+right+1;
         if(!root) return 0;
-        if(!root->left && !root->right) return 1;
-        int left = countNodes(root->left);
-        int right = countNodes(root->right);
-        return left+right+1;
+        int left = 1;
+        TreeNode *l = root->left;
+        while(l){
+            l=l->left;
+            left++;
+        }
+        int right = 1;
+        TreeNode * r = root->right;
+        while(r){
+            r=r->right;
+            right++;
+        }
+
+        if(left==right) return (1<<left) - 1;
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
